b896d279a4c340365bbef55c6ae1ab46b0780bb4
Raito Hayashi <0802hayashi@gmail.com>
2022-03-09 10:01:28 +0900

    ファイル分割

---
+++ 9cc.h
@@ -0,0 +1,55 @@
+// トークンの種類
+typedef enum {
+  TK_RESERVED, // 記号
+  TK_NUM,      // 整数トークン
+  TK_EOF,      // 入力の終わりを表すトークン
+} TokenKind;
+
+typedef struct Token Token;
+
+// トークン型
+struct Token {
+  TokenKind kind; // トークンの型
+  Token *next;    // 次の入力トークン
+  int val;        // kindがTK_NUMの場合、その数値
+  char *str;      // トークン文字列
+  int len;        // トークンの長さ
+};
+
+// 抽象構文木のノードの種類
+typedef enum {
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_MUL, // *
+  ND_DIV, // /
+  ND_LT,  // <
+  ND_LE,  // <=
+  ND_EQ,  // ==
+  ND_NE,  // != 
+  ND_NUM, // 整数
+} NodeKind;
+
+typedef struct Node Node;
+
+// 抽象構文木のノードの型
+struct Node {
+  NodeKind kind; // ノードの型
+  Node *lhs;     // 左辺
+  Node *rhs;     // 右辺
+  int val;       // kindがND_NUMの場合のみ使う
+};
+
+extern Token *token;
+
+extern char *user_input;
+
+Token *tokenize(char *p);
+void error(char *fmt, ...);
+Node *expr();
+Node *equal();
+Node *rel();
+Node *add();
+Node *mul();
+Node *unary();
+Node *primary();
+void gen(Node *node);
